b408749952b46f31275be6b8cc6d7fa6939b9eb6
OPT3D <daveketron@gmail.com>
2022-12-01 11:30:22 -0500

    Finished the walkthrough

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import styles from '../style'
+import Button from './Button'
 
-const CTA = () => {
-  return (
-    <div>
-      
-    </div>
+const CTA = () =>  (
+    <section className={`${styles.flexCenter} ${styles.marginY} ${styles.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
+      <div className='flex-1 flex flex-col'>
+        <h2 className={styles.heading2}>Let's try our service now!</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta quam dignissimos neque consequatur commodi, ipsum deserunt fugit ex nihil odit non suscipit voluptatem sit quas quis eum perferendis quae laborum?</p>
+      </div>
+      <div>
+        <Button className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}/>
+      </div>
+    </section>
   )
-}
+
 
 export default CTA
--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import {card} from '../assets'
+import styles, {layout} from '../style'
+import Button from './Button'
 
-const CardDeal = () => {
-  return (
-    <div>
-      CardDeal
-    </div>
+const CardDeal = () =>  (
+    <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Find a better card <br className='sm:block hidden'/> deal in a few easy steps. </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos tenetur distinctio, iste voluptatum dolores placeat nihil esse in cumque quod. Ratione cupiditate distinctio illum sed magnam. Esse sequi nesciunt consequuntur.</p>
+        <Button styles="mt-10"/>
+      </div>
+      <div className={layout.sectionImg}>
+        <img src={card} alt="card" className='w-[100%] h-[100%]' />
+      </div>
+    </section>
   )
-}
+
 
 export default CardDeal
